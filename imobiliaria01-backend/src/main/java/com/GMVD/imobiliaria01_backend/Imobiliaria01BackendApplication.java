45579e1c44991b6a08b9a77cb684855ad301eadd
Gus <gustavomnicolai2004@gmail.com>
2026-03-29 15:27:28 -0300

    feat: setup inicial do spring boot

---
+++ imobiliaria01-backend/src/main/java/com/GMVD/imobiliaria01_backend/Imobiliaria01BackendApplication.java
@@ -0,0 +1,13 @@
+package com.GMVD.imobiliaria01_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Imobiliaria01BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Imobiliaria01BackendApplication.class, args);
+	}
+
+}
---
+++ imobiliaria01-backend/src/test/java/com/GMVD/imobiliaria01_backend/Imobiliaria01BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GMVD.imobiliaria01_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Imobiliaria01BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
