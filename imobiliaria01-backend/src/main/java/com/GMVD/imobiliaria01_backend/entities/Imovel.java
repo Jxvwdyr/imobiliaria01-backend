5280b3ddd70be55c566dc650d3a7eb314dc407a4
Gus <gustavomnicolai2004@gmail.com>
2026-03-29 16:39:35 -0300

    primeiros packages

---
+++ imobiliaria01-backend/src/main/java/com/GMVD/imobiliaria01_backend/entities/Imovel.java
@@ -0,0 +1,29 @@
+package com.GMVD.imobiliaria01_backend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "imoveis")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Imovel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String titulo;
+
+    private String descricao;
+
+    @Column(nullable = false)
+    private Double preco;
+
+    private Integer quartos;
+}
